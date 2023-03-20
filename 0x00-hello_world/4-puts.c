9c82eeee3a309f6a6f02dc51862198a17f0219a7
Khalil Meftah <khalile.meftah@gmail.com>
2023-03-20 22:08:32 +0000

    Add files via upload

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : '4.Hello, puts'
+ *
+ * @void : The main function takes no parameters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
